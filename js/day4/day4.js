b847642d21c24211d7eb85eb699e95c7806e34bc
Dalibor Babić <124865552+laddeeth@users.noreply.github.com>
2023-12-04 10:11:10 +0100

    Finalized Day 4 Part 1 & 2

--- js/day4/day4.js
+++ js/day4/day4.js
@@ -4,9 +4,9 @@ const buffer = fs.readFileSync('js/day4/input.txt');
 const inputText = buffer.toString();
 const inputArray = inputText.split('\n');
 
-part1();
+console.log('Day 4 - Part 1:', part1(), 'Part 2:', part2());
 
-//Part 1
+//-------------------------------------------------------------PART 1
 function part1() {
   let totalPoints = 0;
 
@@ -49,7 +49,56 @@ function part1() {
     //Add up points
     totalPoints += points;
   }
-  console.log(totalPoints);
+  return totalPoints;
 }
 
-function part2() {}
+//-------------------------------------------------------------PART 2
+function part2() {
+  let allMatches = 0;
+  let matchArray = [];
+  //For each row
+  for (let x = 0; x < inputArray.length; x++) {
+    let rowArray = inputArray[x].substring(10).split('|');
+    winningArray = rowArray[0].split(' ');
+    myNumbersArray = rowArray[1].split(' ');
+
+    //Sanitize arrays
+    for (let x = 0; x < winningArray.length; x++) {
+      if (winningArray[x] == '') {
+        winningArray[x] = -1;
+      } else {
+        winningArray[x] = parseInt(winningArray[x]);
+      }
+    }
+    for (let x = 0; x < myNumbersArray.length; x++) {
+      if (myNumbersArray[x] == '') {
+        myNumbersArray[x] = -2;
+      } else {
+        myNumbersArray[x] = parseInt(myNumbersArray[x]);
+      }
+    }
+    //Find matches
+    let totalMatches = 0;
+    for (let x = 0; x < winningArray.length; x++) {
+      for (let y = 0; y < myNumbersArray.length; y++) {
+        if (winningArray[x] == myNumbersArray[y]) {
+          totalMatches++;
+        }
+      }
+    }
+    matchArray.push([x, totalMatches]);
+  }
+  //Calculate total copies
+  let x = 0;
+  while (x < matchArray.length) {
+    allMatches += matchArray[x][1];
+
+    let start = matchArray[x][0] + 1;
+    let end = matchArray[x][0] + matchArray[x][1];
+    for (start; start <= end; start++) {
+      matchArray.push(matchArray[start]);
+    }
+    x++;
+  }
+  return matchArray.length;
+}
