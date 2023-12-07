b98695bfce546411d39d96c3985ae9597fa1c09d
Dalibor Babić <124865552+laddeeth@users.noreply.github.com>
2023-12-07 09:03:53 +0100

    Coming along with part 2, categorized and now working on sort rank

--- js/day7/day7.js
+++ js/day7/day7.js
@@ -1,4 +1,4 @@
-textInput = `32T3K 765
+const textInput = `32T3K 765
 T55J5 684
 KK677 28
 KTJJT 220
@@ -13,44 +13,81 @@ let twoPair = [];
 let pair = [];
 let highCard = [];
 
-//Svar ska bli 6440
+//Answer shoud be 6440
 
-arrayInput = textInput.split('\n');
+const arrayInput = textInput.split('\n');
 
 //Put together array of hands with sorted cards
 for (let x = 0; x < arrayInput.length; x++) {
   arrayInput[x] = arrayInput[x].split(' ');
   arrayInput[x][1] = parseInt(arrayInput[x][1]);
-  arrayInput[x] = sortCards(arrayInput[x]);
+  arrayInput[x] = arrayInput[x];
 
-  console.log(arrayInput[x]);
+  //Categorize hands
+  if (isFiveKind(arrayInput[x])) {
+    fiveKind.push(arrayInput[x]);
+  } else if (isFourKind(arrayInput[x])) {
+    fourKind.push(arrayInput[x]);
+  } else if (isFullHouse(arrayInput[x])) {
+    fullHouse.push(arrayInput[x]);
+  } else if (isThreeKind(arrayInput[x])) {
+    threeKind.push(arrayInput[x]);
+  } else if (isTwoPair(arrayInput[x])) {
+    twoPair.push(arrayInput[x]);
+  } else if (isPair(arrayInput[x])) {
+    pair.push(arrayInput[x]);
+  } else {
+    highCard.push(arrayInput[x]);
+  }
 }
 
+sortHands(twoPair);
+
+//Categorize Functions
 function isFiveKind(cards) {
-  return cards[0].match(/(.)\1{4}/) ? true : false;
+  tempCards = sortCards(cards);
+  return tempCards[0].match(/(.)\1{4}/) ? true : false;
 }
 
 function isFourKind(cards) {
-  return cards[0].match(/(.)\1{3}/) ? true : false;
+  tempCards = sortCards(cards);
+  return tempCards[0].match(/(.)\1{3}/) ? true : false;
 }
 
 function isFullHouse(cards) {
-  return cards[0].match(/(.)\1{4}/) ? true : false;
+  tempCards = sortCards(cards);
+  return tempCards[0].match(/(.)\1{2}(.)\2{1}|(.)\3{1}(.)\4{2}/) ? true : false;
 }
 
 function isThreeKind(cards) {
-  return cards[0].match(/(.)\1{4}/) ? true : false;
+  tempCards = sortCards(cards);
+  return tempCards[0].match(/(.)\1{2}/) ? true : false;
 }
 
 function isTwoPair(cards) {
-  return cards[0].match(/(.)\1{4}/) ? true : false;
+  tempCards = sortCards(cards);
+  return tempCards[0].match(/(.)\1{1}.?(.)\2{1}/) ? true : false;
 }
 
 function isPair(cards) {
-  return cards[0].match(/(.)\1{4}/) ? true : false;
+  tempCards = sortCards(cards);
+  return tempCards[0].match(/(.)\1{1}/) ? true : false;
 }
 
+//Sorting Functions
 function sortCards(cards) {
   cards[0] = cards[0].split('').sort().join('');
   return cards;
+}
+
+function sortHands(arrayOfHands) {
+  convertedArray = [];
+
+  let x = 0;
+  //Build converted array [cards in array by value, index number in original array]
+  arrayOfHands.forEach((element) => {
+    convertedArray.push([element[0].split(''), x]);
+    x++;
+  });
+  console.log(convertedArray);
 }
